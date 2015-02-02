b17d1ed5f66578432f8462df767ca7917eba8ddd
Fu-Hsi <markac@home.pl>
2015-02-02 19:49:54 +0100

    Update phpbb-selective-quoting.js

--- phpbb-selective-quoting.js
+++ phpbb-selective-quoting.js
@@ -12,23 +12,21 @@
 	    if (selObj && selObj != '') {
 		event.preventDefault();
 
-			postBody = event.target.parentNode.parentNode.parentNode,
-			author = postBody.querySelectorAll('.author a')[1].innerHTML,
-			q = '[quote="' + author + '"]' + selObj + '[/quote]';
+		postBody = event.target.parentNode.parentNode.parentNode;
+		author = postBody.querySelectorAll('.author a')[1].innerHTML;
+		q = '[quote="' + author + '"]' + selObj + '[/quote]';
 
 		editorDiv.style.display = 'block';
 
 		msgText.value = msgText.value + q + "\r\n";
 		msgText.focus();
-
 	    }
 	}
     };
 
     if (editorDiv) {
 	msgText = editorDiv.querySelector('textarea[name="message"]');
-	qButtons = document
-		.querySelectorAll('a[href*="posting.php?mode=quote"]');
+	qButtons = document.querySelectorAll('a[href*="posting.php?mode=quote"]');
 	len = qButtons.length;
 
 	while (len--) {
